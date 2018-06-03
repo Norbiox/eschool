24a77438356c10927152276e1e3e6cfc93980c0a
Norbert Chmiel <norberto9411@gmail.com>
2018-06-03 21:30:21 +0200

    login logout worked out

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
@@ -7,6 +7,6 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    url(r'^login/$', auth_views.login, {'template_name': 'home/login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    re_path(r'^login/$', auth_views.login, {'template_name': 'home/login.html'}, name='login'),
+    re_path(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
 ]
